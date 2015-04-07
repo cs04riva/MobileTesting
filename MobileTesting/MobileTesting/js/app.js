ee03454a7f836104415a415694c2ebbe482f3eea
cs04riva <chrisrivadeneira@outlook.com>
2015-04-06 22:47:07 -0400

    Working with AngularJS - GemStore Code School Example

---
+++ MobileTesting/MobileTesting/js/app.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'This is the description of a gem!',
+            canPurchase: true,
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: ". . .",
+            canPurchase: false,
+        }
+    ]
+})();
